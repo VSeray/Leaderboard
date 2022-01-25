b000f462ccfba75ee8161126cc0484253f04ee69
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-01-25 11:37:07 +0200

    Add score add element

---
+++ src/ScoreAdd.js
@@ -0,0 +1,17 @@
+const scoreAdd = () => {
+  const addForm = document.querySelector('.form-add');
+  const name = document.getElementById('name');
+  const score = document.getElementById('score');
+
+  addForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    saveToApi(gameID, name.value, score.value)
+      .then((response) => {
+        if (response.result === 'Leaderboard score created correctly.') {
+          window.location.reload();
+        }
+      });
+  });
+};
+
+export default scoreAdd;
